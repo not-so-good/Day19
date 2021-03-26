78f1f25f1fa40395ea3b0eff2fb1ba14549d760a
not-so-good <sanjin.suljanovic@gmail.com>
2021-03-26 16:37:47 +0100

    ex04 Destructuring

---
+++ ex04/src/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react"; //Change this line
+import ReactDOM from "react-dom";
+import "./index.css";
+
+class App extends Component { //Change this line
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello, React!</h1>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
